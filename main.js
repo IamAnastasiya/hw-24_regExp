26bf73f7667493c4e13cf22aee4d9e86e40e8286
Nastasiia Balabukha <50119285+IamAnastasiya@users.noreply.github.com>
2020-12-16 16:46:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,16 @@
 let text1 = "привет 12 ываыва34 98 3423ыавыа"; //12, 98
 let text2 = "10 ываыв 8 234ы"; // 10, 8
 let text3 = "декабрь12 2020 выходной12фф 74 2пр2 77"; //2020, 74, 77
-let text4 = "12текст блабла 10 бла12бла 7"; // 10, 7
+let text4 = "12текст блабла 123 10 бла12бла 7"; // 123, 10, 7
 
 function findDetachedNumbers (text) {
-    let regexp = /(^|\s)\d+(\s|$)/g;
-    console.log(text.match(regexp));
-    return text.match(regexp);
+    // let regexp = /\b\d+\b/g; // будет работать только для алфавитов, основанных на латинице
+    let regexp = /(^|\s)\d+(?=\s|$)/g
+    let result = (text.match(regexp).map(item => Number(item)));
+    console.log(result);
+    return result;
 }
+
 
 findDetachedNumbers (text1);
 findDetachedNumbers (text2);
@@ -19,9 +22,9 @@ findDetachedNumbers (text4);
 
 //2nd
 function validateApiAddress (address) {
-    // let regexp = /^\d+\.\d+\.\d+\.\d+$/; - если без проверки диапазона цифр
-    let regexp = /^(([0-1]?[0-9]?[0-9])|([2][0-4][0-9])|(25[0-5]))\.(([0-1]?[0-9]?[0-9])|([2][0-4][0-9])|(25[0-5]))\.(([0-1]?[0-9]?[0-9])|([2][0-4][0-9])|(25[0-5]))\.(([0-1]?[0-9]?[0-9])|([2][0-4][0-9])|(25[0-5]))$/;
+    let regexp = /^((([0-1]?[0-9]?[0-9])|([2][0-4][0-9])|(25[0-5]))\.){3}(([0-1]?[0-9]?[0-9])|([2][0-4][0-9])|(25[0-5]))$/;
     console.log(regexp.test(address));
+    return regexp.test(address);
 }
 
 // ^$ - ограничивают начало и конец строки
@@ -32,7 +35,8 @@ function validateApiAddress (address) {
 // (1е число - (2), далее (0-4) и последнее (0-9) )
 //                                 или
 // (первые 2 числа - (25), далее (0-5))
-// между диапазонами - экранированная точка
+//  повторяю трижды с экранированной точной и один раз без нее
+
 
 validateApiAddress ("127.0.0.0");
 validateApiAddress ("127.0.155.255");
